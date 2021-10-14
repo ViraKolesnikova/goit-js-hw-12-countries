051b1147c1d2a1a888ddc51d327e811beca7b569
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-10-14 19:37:08 +0300

    homewokrk 12 js done

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,12 @@
 export default
   function fetchCountries(searchQuery) {
   return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
-    .then(response => response.json())
+    .then(response => response.ok ? response.json(): Promise.reject({err: response.status}))
     .then(obj => {
       if (obj.status !== 404) {
        return obj;
       } return err;
     })    
-  
+    .catch(error => console.log('Request Failed:', error));
   }
 
