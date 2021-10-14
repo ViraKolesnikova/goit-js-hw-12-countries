8050b20a00cd0b3508781e107626ef4c1ef381e1
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-10-14 16:50:22 +0300

    without 404 error catch

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,65 @@
-import fetchCountries from "./fetchCountries.js";
+import fetchCountries from './fetchCountries.js';
+import countryList from '../templates/countryList.handlebars';
+import country from '../templates/country.handlebars';
 var debounce = require('lodash.debounce');
+import { alert, notice, info, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const searchInputRef = document.querySelector('.search-input');
+const resultRef = document.querySelector('.search-result');
 
-searchInputRef.addEventListener('input', debounce(onSearch, 500));
+searchInputRef.addEventListener('input', debounce(onSearch, 1000));
 
 function onSearch(event) {
   let searchValue = event.target.value;
-  console.log(searchValue);
+
+  if (searchValue === '') {
+    clearMarkup();
+  } else {
+    fetchCountries(searchValue)
+      .then(data => {
+        clearMarkup();
+        return renderTemplate(data);
+      })
+      .then(markup => {
+        if (markup) {
+          insertMarkup(markup)
+        } return
+      })
+      .catch(err => error({
+      text: 'Nothing was found by your query!',
+      type: 'error',
+      width: '600px',
+      closer: false,
+      sticker: false,
+      delay: 2000
+    }))
+  }
+}
+
+function renderTemplate(promiseResult) {
+  if (promiseResult.length === 1) {
+    return country(promiseResult);
+  } else  if(promiseResult.length > 1 && promiseResult.length < 11) {
+    return countryList(promiseResult);
+  } else if(promiseResult.length > 11){
+     error({
+      text: 'Too many matches found. Please enter more specific query!',
+      type: 'error',
+      width: '600px',
+      closer: false,
+      sticker: false,
+      delay: 2000
+    });
+  } else {
+    return
+  }
+}
+
+function insertMarkup(markup) {
+  resultRef.insertAdjacentHTML('afterbegin', markup);
+}
+
+function clearMarkup() {
+  resultRef.innerHTML = '';
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,12 @@
 export default
   function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(response=>console.log)
+  return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+    .then(response => response.json())
+    .then(obj => {
+      if (obj.status !== 404) {
+       return obj;
+      } return err;
+    })    
   
-}
+  }
 
-fetchCountries('india')
