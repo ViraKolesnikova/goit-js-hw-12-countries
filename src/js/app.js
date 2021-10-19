00478f4b6e3cedd7ac0cb02c8522c136b36f5e59
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-10-19 10:52:10 +0300

    js files refactoring, used class for fetchAPI

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import fetchCountries from './fetchCountries.js';
+import CountriesAPI from './fetchCountries.js';
 import countryList from '../templates/countryList.handlebars';
 import country from '../templates/country.handlebars';
 var debounce = require('lodash.debounce');
@@ -10,30 +10,15 @@ const resultRef = document.querySelector('.search-result');
 
 searchInputRef.addEventListener('input', debounce(onSearch, 1000));
 
-function onSearch(event) {
-  let searchValue = event.target.value;
+const countriesAPI = new CountriesAPI();
 
-  if (searchValue === '') {
+function onSearch(event) {
+  countriesAPI._query = event.target.value;
+  
+  if (countriesAPI._query === '') {
     clearMarkup();
   } else {
-    fetchCountries(searchValue)
-      .then(data => {
-        clearMarkup();
-        return renderTemplate(data);
-      })
-      .then(markup => {
-        if (markup) {
-          insertMarkup(markup)
-        } return
-      })
-      .catch(err => error({
-      text: 'Nothing was found by your query!',
-      type: 'error',
-      width: '600px',
-      closer: false,
-      sticker: false,
-      delay: 2000
-    }))
+    fetchQuery().catch(showErrorMessage);
   }
 }
 
@@ -42,15 +27,8 @@ function renderTemplate(promiseResult) {
     return country(promiseResult);
   } else  if(promiseResult.length > 1 && promiseResult.length < 11) {
     return countryList(promiseResult);
-  } else if(promiseResult.length > 11){
-     error({
-      text: 'Too many matches found. Please enter more specific query!',
-      type: 'error',
-      width: '600px',
-      closer: false,
-      sticker: false,
-      delay: 2000
-    });
+  } else if (promiseResult.length > 11) {
+    showInfoMessage();
   } else {
     return
   }
@@ -62,4 +40,40 @@ function insertMarkup(markup) {
 
 function clearMarkup() {
   resultRef.innerHTML = '';
+}
+
+function fetchQuery() {
+  return countriesAPI.fetchCountries()
+    .then(data => {
+      clearMarkup();
+      return renderTemplate(data);
+    })
+    .then(markup => {
+      if (markup) {
+        insertMarkup(markup)
+      } return
+    })
+    
+}
+
+function showErrorMessage() {
+  error({
+    text: 'Nothing was found by your query!',
+    type: 'error',
+    width: '600px',
+    closer: false,
+    sticker: false,
+    delay: 2000
+  })
+}
+
+function showInfoMessage() {
+  notice({
+    text: 'Too many matches found. Please enter more specific query!',
+    type: 'notice',
+    width: '600px',
+    closer: false,
+    sticker: false,
+    delay: 2000
+  })  
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,18 @@
-export default
-  function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+export default class CountriesAPI{
+  constructor() {
+    this._query = ''
+  }
+
+  get query(){
+    return this._query;
+  }
+
+  set query(newQuery){
+    return this._query = newQuery;
+  }
+
+  fetchCountries() {
+  return fetch(`https://restcountries.com/v2/name/${this._query}`)
     .then(response => response.ok ? response.json(): Promise.reject({err: response.status}))
     .then(obj => {
       if (obj.status !== 404) {
@@ -9,4 +21,8 @@ export default
     })    
     .catch(error => console.log('Request Failed:', error));
   }
+} 
 
+  
+  
+ 
